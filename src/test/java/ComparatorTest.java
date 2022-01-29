4ee085902a84375abe8a6e95791873825445a6c1
maxim.yenalyev <maxim.yenalyev@gmail.com>
2022-01-29 22:30:42 +0200

    performance measuring was added

--- src/test/java/ComparatorTest.java
+++ src/test/java/ComparatorTest.java
@@ -1,8 +1,10 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 class ComparatorTest {
@@ -13,7 +15,11 @@ class ComparatorTest {
     void bigFileTestAfterRefactoring() {
         List<String> base = CsvReader.readFile(0,"src/test/resources/big_file.csv");
         List<String> whichCompare = CsvReader.readFile(0,"src/test/resources/big_file.csv");
+        long start = System.currentTimeMillis();
         List<String> comparison = Comparator.compareAfterRefactoring(whichCompare,base);
+        long end = System.currentTimeMillis();
+        long diff = end-start;
+        System.out.println("Total time execution - " + diff);
         Assertions.assertEquals(comparison, new ArrayList<>());
     }
 
@@ -21,7 +27,11 @@ class ComparatorTest {
     void bigFileTestBeforeRefactoring() {
         List<String> base = CsvReader.readFile(0,"src/test/resources/big_file.csv");
         List<String> whichCompare = CsvReader.readFile(0,"src/test/resources/big_file.csv");
-        List<String> comparison = Comparator.compareBeforeRefactor(whichCompare,base);
+        long start = System.currentTimeMillis();
+        List<String> comparison = Comparator.compareBeforeRefactoring(whichCompare,base);
+        long end = System.currentTimeMillis();
+        long diff = end-start;
+        System.out.println("Total time execution - " + diff);
         Assertions.assertEquals(comparison, new ArrayList<>());
     }
 
@@ -32,7 +42,7 @@ class ComparatorTest {
         List<String> base = CsvReader.readFile(0,"src/test/resources/test.csv");
         List<String> whichCompare = CsvReader.readFile(0,"src/test/resources/test_2.csv");
         List<String> comparison = Comparator.compareAfterRefactoring(whichCompare,base);
-        Assertions.assertEquals(comparison, new ArrayList<>());
+        Assertions.assertEquals(Arrays.asList("4","5","6","7"), comparison);
     }
 
     @Test
@@ -40,7 +50,8 @@ class ComparatorTest {
         List<String> base = CsvReader.readFile(0,"src/test/resources/test.csv");
         List<String> whichCompare = CsvReader.readFile(0,"src/test/resources/test_2.csv");
         List<String> comparison = Comparator.compareAfterRefactoring(base, whichCompare);
-        Assertions.assertEquals(comparison, Arrays.asList("4","5","6","7"));
+        Assertions.assertEquals(new ArrayList<>(), comparison);
+
     }
 
 
@@ -48,15 +59,15 @@ class ComparatorTest {
     void compareBeforeRefactoring() {
         List<String> base = CsvReader.readFile(0,"src/test/resources/test.csv");
         List<String> whichCompare = CsvReader.readFile(0,"src/test/resources/test_2.csv");
-        List<String> comparison = Comparator.compareAfterRefactoring(whichCompare,base);
-        Assertions.assertEquals(comparison, new ArrayList<>());
+        List<String> comparison = Comparator.compareBeforeRefactoring(whichCompare,base);
+        Assertions.assertEquals(Arrays.asList("4","5","6","7"), comparison);
     }
 
     @Test
     void compareBeforeRefactoring_2() {
         List<String> base = CsvReader.readFile(0,"src/test/resources/test.csv");
         List<String> whichCompare = CsvReader.readFile(0,"src/test/resources/test_2.csv");
-        List<String> comparison = Comparator.compareAfterRefactoring(base, whichCompare);
-        Assertions.assertEquals(comparison, Arrays.asList("4","5","6","7"));
+        List<String> comparison = Comparator.compareBeforeRefactoring(base, whichCompare);
+        Assertions.assertEquals(new ArrayList<>(), comparison);
     }
 }
